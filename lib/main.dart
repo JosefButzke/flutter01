7d83942de3166f9decaae775b24515dbc89a7f22
JosefButzke <josefbr123@gmail.com>
2021-06-21 23:25:38 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      theme: ThemeData(primaryColor: Colors.blue), home: HomePage()));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var count = 0;
+
+  void increment() {
+    count++;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("CoinMarketCap"),
+        elevation: 0,
+      ),
+      body:
+          Center(child: Text("CONTADOR\n$count", textAlign: TextAlign.center)),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          increment();
+        },
+      ),
+    );
+  }
+}
